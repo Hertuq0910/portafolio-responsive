85aac64c039293d7f2253f6ad6f6b7a8533c920d
Hertuq0910 <118753877+Hertuq0910@users.noreply.github.com>
2023-03-08 16:58:16 -0500

    Versionamiento 1.1. cv

---
+++ components/autoType.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".auto-input", {
+  strings: [
+    "Desarrollador Web",
+    "Desarrollador Fullstack",
+    "Web Developer",
+    "Fullstack Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
